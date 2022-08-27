e91d1bf254e9ec51eb0941525ea3a6f4d2ff0829
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-08-27 14:35:15 -0500

    adding files

---
+++ Develop/script.js
@@ -0,0 +1,22 @@
+// Assignment code here
+
+var passwordOptions = ["abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-="];
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+  alert("Please choose between at least 8 characters and no more than 128 characters");
+}
