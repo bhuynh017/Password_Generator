e061e542d8d7241a294a1582c975d6967d75b6e8
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-08-29 17:59:56 -0500

    updated script

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,16 @@
 // Assignment code here
 // javascript error : generatePassword is not defined.
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate"); // targets generate ID 
+var passwordLength = 8;
+var choiceArr = [];
 
-function generatePassword() {
-    console.log("test")
-}
+var specialOptions = ["!", "#", "$", "%", "~", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "|"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate"); // targets generate ID in html
 
 // Write password to the #password input
 function writePassword() {
@@ -17,5 +21,14 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+    console.log("test")
+
+    return "Password test";
+}
+
+function userPrompts () {
+    passwordLength = prompt("How many characters would you like your password to contain?");
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); //when someone clicks the button it will call the writePassword function
