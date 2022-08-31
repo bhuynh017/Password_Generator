bea3caa9e4015311e437fb2b2e9bed905c94efdd
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-08-30 19:52:44 -0500

    updated files

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,41 @@
 // Assignment code here
 // javascript error : generatePassword is not defined.
 
-var passwordLength = 8;
+var passwordLength = [];
 var choiceArr = [];
 
-var specialOptions = ["!", "#", "$", "%", "~", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "|"];
+// below are arrays
+var specialOptions = ["!", "#", "$", "%", "~", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "|"]; 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate"); // targets generate ID in html
+var generateBtn = document.querySelector("#generate"); // targets generate ID in html. This makes the button work on the page.
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); // there is no generatePassword 
-  var passwordText = document.querySelector("#password"); 
-
-  passwordText.value = password; //displays it onto the screen
-
+    var accuratePrompts = userPrompts();
+    var passwordText = document.querySelector("#password"); 
+    
+    if (accuratePrompts) {
+        var newPassword = generatePassword(); // there is no generatePassword 
+       
+  passwordText.value = newPassword; //displays it onto the screen
+    }
 }
 
 function generatePassword() {
-
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+        var randomLetter = Math.floor(Math.random() * choiceArr.length)
+        password = password + choiceArr[randomLetter];
+    }
+    return password;
 }
 
 function userPrompts () {
+    choiceArr = [];
     passwordLength = parseInt(prompt("How many characters would you like your password to contain?"));
 
     if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) { //NaN --- not a number
